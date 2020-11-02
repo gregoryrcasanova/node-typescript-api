c5855b26d5facf43508a0b0a20af535664b53aa4
Gregory Casanova <gregorynewhouse@hotmail.com>
2020-11-02 15:02:23 -0300

    Add chmod -x to deploy file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/.ssh
-echo -e "${SSH_KEY}//_/\\n" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
+echo -e "${SSH_KEY}//_/\\n" > ~/.ssh/id_rsa_tutorial
+chmod 600 ~/.ssh/id_rsa_tutorial
 
 ssh-keyscan -p 9922 -t rsa geonosis.deploy.umbler.com 2>&1 >> ~/.ssh/known_hosts
