ae7f05ec5d4c35b90af3893f9f1f6f94a3134186
Gregory Casanova <gregorynewhouse@hotmail.com>
2020-11-02 12:12:06 -0300

    up the api of node-typescripi-api course created by Waldemar Neto, to continue the next steps of the course

---
+++ src/server.ts
@@ -0,0 +1,62 @@
+import { Server } from '@overnightjs/core'
+import * as database from '@src/database'
+import bodyParser from 'body-parser'
+import { Application } from 'express'
+import { BeachesController } from './controllers/beaches'
+import { ForecastController } from './controllers/forecast'
+import { UsersController } from './controllers/users'
+import './util/module-alias'
+
+export class SetupServer extends Server {
+  /*
+   * same as this.port = port, declaring as private here will
+   * add the port variable to the SetupServer instance
+   */
+  constructor (private port = 3000) {
+    super()
+  }
+
+  /*
+   * We use a different method to init instead of using the constructor
+   * this way we allow the server to be used in tests and normal initialization
+   */
+  public async init (): Promise<void> {
+    this.setupExpress()
+    this.setupControllers()
+    await this.databaseSetup()
+  }
+
+  private setupExpress (): void {
+    this.app.use(bodyParser.json())
+    this.setupControllers()
+  }
+
+  private setupControllers (): void {
+    const forecastController = new ForecastController()
+    const beachesController = new BeachesController()
+    const usersController = new UsersController()
+    this.addControllers([
+      forecastController,
+      beachesController,
+      usersController,
+    ])
+  }
+
+  public getApp (): Application {
+    return this.app
+  }
+
+  private async databaseSetup (): Promise<void> {
+    await database.connect()
+  }
+
+  public async close (): Promise<void> {
+    await database.close()
+  }
+
+  public start (): void {
+    this.app.listen(this.port, () => {
+      console.info('Server listening on port: ' + this.port)
+    })
+  }
+}
